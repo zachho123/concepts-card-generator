180400bb6b82063009d66f67d4810617443ab11c
zachho123 <zachho123@gmail.com>
2020-08-26 14:29:45 -0700

    Updates

---
+++ precache-manifest.9fa0c998511b9ebcc462cc0f19ee1a41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b6db127976ce2c42f1a70675c26682",
+    "url": "/concepts-card-generator/index.html"
+  },
+  {
+    "revision": "842edaaae6ba2b9c4927",
+    "url": "/concepts-card-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b83a24f73d762ab13b5",
+    "url": "/concepts-card-generator/static/js/2.598ac80d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/concepts-card-generator/static/js/2.598ac80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842edaaae6ba2b9c4927",
+    "url": "/concepts-card-generator/static/js/main.3aaadf59.chunk.js"
+  },
+  {
+    "revision": "6f0b13ca05b8836541d6",
+    "url": "/concepts-card-generator/static/js/runtime-main.e9cbc325.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/concepts-card-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
