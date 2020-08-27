17f3465a9c9aee77270e78c63fc9468a8191e7e5
zachho123 <zachho123@gmail.com>
2020-08-26 20:42:58 -0700

    Work on canvas card design

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
+import { fabric } from 'fabric';
+
+// Assets
 import logo from '../assets/img/logo.svg';
 import '../styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numCards: 68
+    };
+  }
+
+  componentDidMount() {
+    let canvas = new fabric.StaticCanvas(
+      document.getElementById('canvas'),
+      {
+        width: 500,
+        height: 700,
+        backgroundColor: '#FFFAF0'
+      }
+    );
+
+    let cardBorder = new fabric.Rect({
+      left: 10,
+      top: 10,
+      width: 480,
+      height: 680,
+      fill: '#FFFAF0',
+      stroke: 'blue',
+      strokeWidth: 1
+    });
+
+    let easyBorder = new fabric.Rect({
+      left: 20,
+      top: 20,
+      width: 440,
+      height: 200,
+      fill: '#FFFAF0',
+      stroke: 'green',
+      strokeWidth: 2
+    });
+
+    let medBorder = new fabric.Rect({
+      left: 20,
+      top: 230,
+      width: 440,
+      height: 200,
+      fill: '#FFFAF0',
+      stroke: 'blue',
+      strokeWidth: 2
+    });
+
+    let hardBorder = new fabric.Rect({
+      left: 20,
+      top: 440,
+      width: 440,
+      height: 200,
+      fill: '#FFFAF0',
+      stroke: 'red',
+      strokeWidth: 2
+    });
+    
+    canvas.add(cardBorder, easyBorder, medBorder, hardBorder);
+  }
+
+  render() {
+    return (
+      <canvas id="canvas" className="card-canvas"></canvas>
+    );
+  }
 }
 
 export default App;
